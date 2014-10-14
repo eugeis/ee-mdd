da9343a86f3ce00b6caebeb58584c1054e9bab93
elektr0n1k <n.cappelmann@tu-bs.de>
2014-10-14 20:09:52 +0200

    Added PropFactory and Index

---
+++ ee-mdd-core/src/main/groovy/ee/mdd/builder/PropFactory.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ee.mdd.builder
+
+import ee.mdd.model.component.DataTypeProp
+import ee.mdd.model.component.Prop
+
+
+/**
+ *
+ * @author Eugen Eisler
+ */
+class PropFactory extends CompositeFactory {
+
+  public PropFactory() {
+    super()
+    this.beanClass = Prop
+  }
+
+  Object newInstance(AbstractFactoryBuilder builder, Object name, Object value, Map attributes) throws InstantiationException, IllegalAccessException {
+    if (checkValue(name, value)) {
+      return value
+    }
+    def parent = builder.getParent()
+    def ret
+    if(DataTypeProp.isInstance(parent)) {
+      ret = new DataTypeProp()
+    } else {
+      ret = new Prop()
+    }
+
+    if(value != null) {
+      //attributes[valueProperty] = value
+      ret[valueProperty] = value
+    }
+    ret
+  }
+}
