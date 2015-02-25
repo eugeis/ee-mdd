4d38dea01343f1dd0f26e6cb58c1317424d83a60
elektr0n1k <n.cappelmann@tu-bs.de>
2015-02-25 12:06:32 +0100

    added cg facet

---
+++ ee-mdd-core/src/main/groovy/ee/mdd/model/component/java/Cg.groovy
@@ -0,0 +1,13 @@
+/*
+ * Controlguide
+ * Copyright (c) Siemens AG 2015, All Rights Reserved, Confidential
+ */
+package ee.mdd.model.component.java
+
+import ee.mdd.model.component.Facet
+
+class Cg extends Facet {
+  protected Map nameToNamespace() {
+    ['Service' : 'com.siemens.ra.cg.pl.common.base.annotations', 'Environment' : 'com.siemens.ra.cg.pl.common.base.cdi.env', 'SupportsEnvironment' : 'com.siemens.ra.cg.pl.common.base.cdi.env']
+  }
+}
