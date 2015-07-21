808382b9c5978cbb2abe8e0cd1550835feced5e3
Jonas Möller <moeller-jonas@web.de>
2015-07-21 10:46:10 +0200

    Start working on AngularJS templates

---
+++ ee-mdd-gradle/src/test/groovy/ee/mdd/gradle/task/GenerateJsTest.groovy
@@ -0,0 +1,128 @@
+package ee.mdd.gradle.task
+
+import org.junit.Test
+
+import ee.mdd.generator.java.GeneratorForJava;
+import ee.mdd.model.component.Facet
+import ee.mdd.model.component.Model
+
+class GenerateJsTest {
+	GeneratorForJava generator = new GeneratorForJava()
+
+	@Test
+	void testExtendModel() {
+
+		Facet facet = generator.builder.build {
+			java {
+				common()
+				//        cdi()
+				//        ejb()
+				//        jms()
+				//        jpa()
+				//        test()
+				//        ee()
+				//        cg()
+			}
+		}
+
+		Model model = generator.builder.model ('MddExample', key: 'cg', namespace: 'ee.mdd', uri: 'cg.test') {
+
+			component('MddExampleComponent', key: 'me', namespace: 'example', artifact: 'ee-mdd_example') {
+
+				module('shared') {
+
+					enumType('TaskType', defaultLiteral: 'Unknown', desc: '''Defines the type of a task''') {
+						prop('code', type: 'int')
+
+						constr { param(prop: 'code') }
+
+						lit('Unknown', body: '-1')
+						lit('Open', body: '1')
+						lit('Closed', body: '2')
+					}
+
+					entity('Comment', attributeChangeFlag: true) {
+						prop('id', type: 'Long',  unique: true, primaryKey: true)
+						prop('testTask', type: 'Task', opposite: 'comment')
+						prop('testProp', type: 'Task', multi: true)
+						prop('dateOfCreation', type: 'Date')
+						prop('newTask', type: 'Task')
+
+						constr {}
+
+						commands {
+							delete { param(prop: 'dateOfCreation') }
+						}
+					}
+
+					entity('Task', attributeChangeFlag: true, ordered: true) {
+						prop('id', type: 'Long', primaryKey: true, unique: true)
+						prop('comment', type: 'Comment', opposite: 'testTask')
+						prop('created', type: 'Date', unique: true)
+						prop('closed', type: 'Date', index: true)
+						prop('actions', type: 'TaskAction', multi: true )
+						prop('size', type: 'int')
+						prop('order', type: 'Long', xml: 'false')
+
+						constr {}
+
+						constr {
+							param(prop: 'id')
+							param(prop: 'created', value: '#newDate')
+						}
+
+						constr {
+							param(prop: 'actions')
+							param(prop: 'created')
+							param(prop: 'closed')
+						}
+
+						op('hello', body: '#testBody') {
+							param('Test', type: 'String')
+							param('countdown', type: 'int')
+						}
+
+						index( props: [
+							'comment',
+							'created'
+						])
+
+						finder {
+							findBy { param(prop: 'comment' ) }
+							count { param(prop: 'created') }
+							exist {
+								param(prop: 'created')
+								param(prop: 'closed')
+							}
+						}
+
+						commands {
+							delete { param(prop: 'closed') }
+						}
+					}
+
+					entity('TaskAction', description: '''The entity that represents the action''') {
+						prop('id', type:'Long', unique:true, primaryKey:true)
+						prop('task', type:'Task', opposite:'actions', description: '')
+						prop('name', type:'String')
+					}
+				}
+
+				module('ui', namespace: 'ui') {
+				}
+			}
+		}
+
+		model.add(facet)
+		facet.extendModel(model)
+
+		generator.generate(model, new File('temp'))
+
+		model.extend {
+			component('Foo') {
+			}
+		}
+
+		println model
+	}
+}
--- ee-mdd-templates/src/main/resources/templates/java/common.groovy
+++ ee-mdd-templates/src/main/resources/templates/java/common.groovy
@@ -35,120 +35,142 @@ import ee.mdd.model.component.Facade
 
 templates ('common') {
 
-  //model
-  templates ('modelApi',
-  items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
+	//model
+	templates ('modelApi',
+	items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
 
-    template('ifcEntity', appendName: true, body: '''<% if(c.item.base) { c.className = item.n.cap.base } else { c.className = item.cap } %><% c.serializable = true %>${macros.generate('ifcEntity', c)}''')
-    template('ifcEntityExtends', appendName: true, body: '''<% if(c.item.base) { %>${macros.generate('ifcExtends', c)}<% } %>''')
-  }
+		template('ifcEntity', appendName: true, body: '''<% if(c.item.base) { c.className = item.n.cap.base } else { c.className = item.cap } %><% c.serializable = true %>${macros.generate('ifcEntity', c)}''')
+		template('ifcEntityExtends', appendName: true, body: '''<% if(c.item.base) { %>${macros.generate('ifcExtends', c)}<% } %>''')
+	}
 
-  templates ('modelApiBasicType',
-  items: { c -> c.model.findAllRecursiveDown( { BasicType.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
+	templates ('modelApiBasicType',
+	items: { c -> c.model.findAllRecursiveDown( { BasicType.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
 
-    template('ifcBasicType', appendName: true, body: '''<% if(c.item.base) { c.className = item.n.cap.base } else { c.className = item.cap } %> ${macros.generate('ifcBasicType', c)}''')
-    template('ifcBasicTypeExtends', appendName: true, body: '''<% if(c.item.base) { %>${macros.generate('ifcExtends', c)}<% } %>''')
-  }
+		template('ifcBasicType', appendName: true, body: '''<% if(c.item.base) { c.className = item.n.cap.base } else { c.className = item.cap } %> ${macros.generate('ifcBasicType', c)}''')
+		template('ifcBasicTypeExtends', appendName: true, body: '''<% if(c.item.base) { %>${macros.generate('ifcExtends', c)}<% } %>''')
+	}
 
-  templates ('modelImplEntity',
-  items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'impl' ] ) } ) {
+	templates ('modelImplEntity',
+	items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'impl' ] ) } ) {
 
-    template('implEntity', appendName: true, body: '''<% c.metas = item.metas; c.serializable = true; if(c.item.base) { c.className = item.n.cap.baseImpl } else { c.className = item.n.cap.impl } %>${macros.generate('implEntity', c)}''')
-    template('implEntityExtends', appendName: true, body: '''<% if(c.item.base) { %><% c.serializable = true; c.className = item.n.cap.impl %>${macros.generate('implEntityExtends', c)}<% } %>''')
-  }
+		template('implEntity', appendName: true, body: '''<% c.metas = item.metas; c.serializable = true; if(c.item.base) { c.className = item.n.cap.baseImpl } else { c.className = item.n.cap.impl } %>${macros.generate('implEntity', c)}''')
+		template('implEntityExtends', appendName: true, body: '''<% if(c.item.base) { %><% c.serializable = true; c.className = item.n.cap.impl %>${macros.generate('implEntityExtends', c)}<% } %>''')
+	}
 
-  templates('cache',
-  items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'cache']) } ) {
+	templates('cache',
+	items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'cache']) } ) {
 
-    template('ifcCache', appendName: true, body: '''<% c.className = item.n.cap.cacheBase %> ${macros.generate('ifcCache', c)}''')
-    template('ifcCacheExtends', appendName: true, body: '''<% c.className = item.n.cap.cache %> ${macros.generate('ifcCacheExtends', c)}''')
-  }
+		template('ifcCache', appendName: true, body: '''<% c.className = item.n.cap.cacheBase %> ${macros.generate('ifcCache', c)}''')
+		template('ifcCacheExtends', appendName: true, body: '''<% c.className = item.n.cap.cache %> ${macros.generate('ifcCacheExtends', c)}''')
+	}
 
-  templates('implCache',
-  items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'cache']) } ) {
+	templates('implCache',
+	items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'cache']) } ) {
 
-    template('implCache', appendName: true, body: '''<% c.className = item.n.cap.cacheBaseImpl %> ${macros.generate('implCache', c)}''')
-    template('implCacheExtends', appendName: true, body: '''<% c.className = item.n.cap.cacheImpl %> ${macros.generate('implCacheExtends', c)}''')
-    template('cacheOverride', appendName: true, body: '''<% c.className = item.n.cap.cacheOverrideBase %><% c.override = true %> ${macros.generate('implCache', c)}''')
-    template('cacheOverrideExtends', appendName: true, body: '''<% c.className = item.n.cap.cacheOverride %><% c.override = true %> ${macros.generate('implCacheExtends', c)}''')
-  }
+		template('implCache', appendName: true, body: '''<% c.className = item.n.cap.cacheBaseImpl %> ${macros.generate('implCache', c)}''')
+		template('implCacheExtends', appendName: true, body: '''<% c.className = item.n.cap.cacheImpl %> ${macros.generate('implCacheExtends', c)}''')
+		template('cacheOverride', appendName: true, body: '''<% c.className = item.n.cap.cacheOverrideBase %><% c.override = true %> ${macros.generate('implCache', c)}''')
+		template('cacheOverrideExtends', appendName: true, body: '''<% c.className = item.n.cap.cacheOverride %><% c.override = true %> ${macros.generate('implCacheExtends', c)}''')
+	}
 
-  templates ('enum',
-  items: { c -> c.model.findAllRecursiveDown( { EnumType.isInstance(it) }) },
-  context: { c -> def enumType = c.item; c.putAll( [ component: enumType.component, module: enumType.module, enumType: enumType ] ) } ) {
+	templates ('enum',
+	items: { c -> c.model.findAllRecursiveDown( { EnumType.isInstance(it) }) },
+	context: { c -> def enumType = c.item; c.putAll( [ component: enumType.component, module: enumType.module, enumType: enumType ] ) } ) {
 
-    template('enum', appendName: true, body: '''${macros.generate('enum', c)}''')
-  }
+		template('enum', appendName: true, body: '''${macros.generate('enum', c)}''')
+	}
 
-  //logic
-  templates ('service',
-  items: { c -> c.model.findAllRecursiveDown( { Facade.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
+	//logic
+	templates ('service',
+	items: { c -> c.model.findAllRecursiveDown( { Facade.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
 
-    template('ifcService', appendName: true, body: '''<% c.className = item.n.cap.base %>${macros.generate('ifcService', c)}''')
-    template('ifcServiceExtends', appendName: true, body: '''<% if (c.item.base) { %><% c.className = item.cap %> ${macros.generate('ifcServiceExtends', c)}<% } %>''')
-  }
+		template('ifcService', appendName: true, body: '''<% c.className = item.n.cap.base %>${macros.generate('ifcService', c)}''')
+		template('ifcServiceExtends', appendName: true, body: '''<% if (c.item.base) { %><% c.className = item.cap %> ${macros.generate('ifcServiceExtends', c)}<% } %>''')
+	}
 
-  templates ('controller',
-  items: { c -> c.model.findAllRecursiveDown( { Controller.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
+	templates ('controller',
+	items: { c -> c.model.findAllRecursiveDown( { Controller.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
 
-    template('ifcController', appendName: true, body: '''<% c.className = "${item.cap}Base" %>${macros.generate('ifcController', c)}''')
-    template('ifcControllerExtends', appendName: true, body: '''<% if (c.item.base) { %><% c.className = item.cap %> ${macros.generate('ifcControllerExtends', c)}<% } %>''')
-  }
+		template('ifcController', appendName: true, body: '''<% c.className = "${item.cap}Base" %>${macros.generate('ifcController', c)}''')
+		template('ifcControllerExtends', appendName: true, body: '''<% if (c.item.base) { %><% c.className = item.cap %> ${macros.generate('ifcControllerExtends', c)}<% } %>''')
+	}
 
-  templates ('container',
-  items: { c -> c.model.findAllRecursiveDown( { Container.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'model'] ) } ) {
+	templates ('container',
+	items: { c -> c.model.findAllRecursiveDown( { Container.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'model'] ) } ) {
 
-    template('ifcContainer', appendName: true, body: '''<% c.className = c.item.n.cap.base %> ${macros.generate('ifcContainer', c)}''')
-    template('ifcContainerExtends', appendName: true, body: '''<% if (c.item.base) { %><% c.className = item.cap %> ${macros.generate('ifcContainerExtends', c)}<% } %>''')
-    template('ifcContainerDelta', appendName: true, body: '''<% c.className = item.n.cap.deltaBase %> ${macros.generate('ifcContainerDelta', c)}''')
-    template('ifcContainerDeltaExtends', appendName: true, body: '''<% c.className = item.n.cap.delta %> ${macros.generate('ifcContainerDeltaExtends', c)}''')
-    template('containerIdsBase', appendName: true, body: '''<% c.className = item.n.cap.idsBase %> ${macros.generate('containerIdsBase', c)}''')
-    template('containerIds', appendName: true, body: '''<% c.className = item.n.cap.ids %> ${macros.generate('containerIds', c)}''')
-    template('implContainerDelta', appendName: true, body: '''<% c.className = item.n.cap.deltaBaseImpl %> ${macros.generate('implContainerDelta', c)}''')
-  }
+		template('ifcContainer', appendName: true, body: '''<% c.className = c.item.n.cap.base %> ${macros.generate('ifcContainer', c)}''')
+		template('ifcContainerExtends', appendName: true, body: '''<% if (c.item.base) { %><% c.className = item.cap %> ${macros.generate('ifcContainerExtends', c)}<% } %>''')
+		template('ifcContainerDelta', appendName: true, body: '''<% c.className = item.n.cap.deltaBase %> ${macros.generate('ifcContainerDelta', c)}''')
+		template('ifcContainerDeltaExtends', appendName: true, body: '''<% c.className = item.n.cap.delta %> ${macros.generate('ifcContainerDeltaExtends', c)}''')
+		template('containerIdsBase', appendName: true, body: '''<% c.className = item.n.cap.idsBase %> ${macros.generate('containerIdsBase', c)}''')
+		template('containerIds', appendName: true, body: '''<% c.className = item.n.cap.ids %> ${macros.generate('containerIds', c)}''')
+		template('implContainerDelta', appendName: true, body: '''<% c.className = item.n.cap.deltaBaseImpl %> ${macros.generate('implContainerDelta', c)}''')
+	}
 
-  templates ('implContainer',
-  items: { c -> c.model.findAllRecursiveDown( {Container.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'impl' ] ) } ) {
+	templates ('implContainer',
+	items: { c -> c.model.findAllRecursiveDown( {Container.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'impl' ] ) } ) {
 
-    template('implContainer', appendName: true, body: '''<% c.className = c.item.n.cap.baseImpl %>${macros.generate('implContainer', c)}''')
-    template('implContainerExtends', appendName: true, body: '''<% if (c.item.base) { %> c.className = c.item.n.cap.impl %>${macros.generate('implContainerExtends', c)}<% } %>''')
-    template('implContainerDeltaExtends', appendName: true, body: '''<% c.className = item.n.cap.deltaImpl %> ${macros.generate('implContainerDeltaExtends', c)}''')
-  }
+		template('implContainer', appendName: true, body: '''<% c.className = c.item.n.cap.baseImpl %>${macros.generate('implContainer', c)}''')
+		template('implContainerExtends', appendName: true, body: '''<% if (c.item.base) { %> c.className = c.item.n.cap.impl %>${macros.generate('implContainerExtends', c)}<% } %>''')
+		template('implContainerDeltaExtends', appendName: true, body: '''<% c.className = item.n.cap.deltaImpl %> ${macros.generate('implContainerDeltaExtends', c)}''')
+	}
 
-  templates ('jmsToCdi',
-  items: { c -> c.model.findAllRecursiveDown( { Channel.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'integ' ] ) } ) {
+	templates ('jmsToCdi',
+	items: { c -> c.model.findAllRecursiveDown( { Channel.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module, subPkg: 'integ' ] ) } ) {
 
-    template('jmsToCdi', appendName: true, body: '''<% if (module.entities || module.configs) { %><% c.className = c.item.n.cap.jmsToCdi %> ${macros.generate('jmsToCdi', c)}<% } %>''')
-    template('jmsToCdiMdb', appendName: true, body: '''<% def cachedContainers = module.containers.findAll { it.controller.cache }%><% if (cachedContainers || module.configs) { %><% c.className = c.item.n.cap.jmsToCdiMdb %> ${macros.generate('jmsToCdiMdb', c)}<% } %>''')
-    template('notificationPlugin', appendName: true, body: '''<% def modules = []; modules.addAll(component.backends.findAll { m -> m.entities }) %><% if(modules) { %><% c.className = component.n.cap.notificationPlugin %> ${macros.generate('notificationPlugin', c)} <% } %> ''')
-  }
+		template('jmsToCdi', appendName: true, body: '''<% if (module.entities || module.configs) { %><% c.className = c.item.n.cap.jmsToCdi %> ${macros.generate('jmsToCdi', c)}<% } %>''')
+		template('jmsToCdiMdb', appendName: true, body: '''<% def cachedContainers = module.containers.findAll { it.controller.cache }%><% if (cachedContainers || module.configs) { %><% c.className = c.item.n.cap.jmsToCdiMdb %> ${macros.generate('jmsToCdiMdb', c)}<% } %>''')
+		template('notificationPlugin', appendName: true, body: '''<% def modules = []; modules.addAll(component.backends.findAll { m -> m.entities }) %><% if(modules) { %><% c.className = component.n.cap.notificationPlugin %> ${macros.generate('notificationPlugin', c)} <% } %> ''')
+	}
 
-  templates ('commandsFinders',
-  items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
+	templates ('commandsFinders',
+	items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item.component, module: c.item.module ] ) } ) {
 
-    template('ifcFinders', appendName: true, body: '''<% if (item.finders && !item.virtual) { %><% c.className = item.n.cap.finders %>${macros.generate('ifcFinders', c)}<% } %>''')
-    template('ifcCommands', appendName: true, body: '''<% if (item.commands && !item.virtual) { %><% c.className = item.n.cap.commands %>${macros.generate('ifcCommands', c)}<% } %>''')
+		template('ifcFinders', appendName: true, body: '''<% if (item.finders && !item.virtual) { %><% c.className = item.n.cap.finders %>${macros.generate('ifcFinders', c)}<% } %>''')
+		template('ifcCommands', appendName: true, body: '''<% if (item.commands && !item.virtual) { %><% c.className = item.n.cap.commands %>${macros.generate('ifcCommands', c)}<% } %>''')
 
-  }
+	}
 
-  templates ('constants',
-  items: { c -> c.model.findAllRecursiveDown( { Component.isInstance(it) }) },
-  context: { c -> c.putAll( [ component: c.item, module: c.item.module, subPkg: 'integ'] ) } ) {
-    // Custom paths for Component templates
-    template('constants', appendName: true, body: '''<% c.className = c.item.n.cap.constantsBase%><% c.path = "ee-mdd_example-shared/src-gen/main/java/${c.item.ns.path}/integ/${c.className}.java" %>${macros.generate('constants', c)}''')
-    //    template('constantsExtends', appendName: true, body: '''<% c.className = c.item.n.cap.constants%> ${macros.generate('constantsExtends', c)}''')
-    template('Ml', appendName: true, body: '''<% c.className = "${item.name}MlBase" %><% c.path = "ee-mdd_example-shared/src-gen/main/java/${c.item.ns.path}/integ/${c.className}.java" %> ${macros.generate('constantsMl', c)}''')
-    template('MlExtends', appendName: true, body: '''<% c.className = "${item.name}Ml" %><% c.path = "ee-mdd_example-shared/src/main/java/${c.item.ns.path}/integ/${c.className}.java" %> ${macros.generate('constantsMlExtends', c)}''')
-  }
+	templates ('constants',
+	items: { c -> c.model.findAllRecursiveDown( { Component.isInstance(it) }) },
+	context: { c -> c.putAll( [ component: c.item, module: c.item.module, subPkg: 'integ'] ) } ) {
+		// Custom paths for Component templates
+		template('constants', appendName: true, body: '''<% c.className = c.item.n.cap.constantsBase%><% c.path = "ee-mdd_example-shared/src-gen/main/java/${c.item.ns.path}/integ/${c.className}.java" %>${macros.generate('constants', c)}''')
+		//    template('constantsExtends', appendName: true, body: '''<% c.className = c.item.n.cap.constants%> ${macros.generate('constantsExtends', c)}''')
+		template('Ml', appendName: true, body: '''<% c.className = "${item.name}MlBase" %><% c.path = "ee-mdd_example-shared/src-gen/main/java/${c.item.ns.path}/integ/${c.className}.java" %> ${macros.generate('constantsMl', c)}''')
+		template('MlExtends', appendName: true, body: '''<% c.className = "${item.name}Ml" %><% c.path = "ee-mdd_example-shared/src/main/java/${c.item.ns.path}/integ/${c.className}.java" %> ${macros.generate('constantsMlExtends', c)}''')
+	}
+
+
+
+
+
+	/*
+	 *
+	 * Javascript templates
+	 *
+	 *
+	 *
+	 *  ${macros.generate('myhtmlfootermacro', c)}
+	 */
+
+
+	templates ('api',
+	items: { c -> c.model.findAllRecursiveDown( { Entity.isInstance(it) }) },
+	context: { c -> def entity = c.item; c.putAll( [ component: entity.component, module: entity.module, entity: entity, subPkg: 'impl' ] ) } ) {
+		template('dings', appendName: true,  body: '''<% c.className = "${item.name}html" %> ${macros.generate('myhtmlheadermacro', c)} ${macros.generate('myhtmlbodymacro', c)}''')
+		//template('dings2', appendName: true,  body: '''<% c.className = "${item.name}js" %> ${macros.generate('myangularmacro', c)}''')
+		//template('dings3', appendName: true,  body: '''<% c.className = "${item.name}css" %> ${macros.generate('mycssmacro', c)}''')
+	}
 }
