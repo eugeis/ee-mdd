cf9a91b807811d7f24a28e3636bb8ed95f58d33c
elektr0n1k <n.cappelmann@tu-bs.de>
2015-08-20 16:34:46 +0200

    Added statemachine facet

---
+++ ee-mdd-templates/src/main/resources/templates/java/sm.groovy
@@ -0,0 +1,21 @@
+package templates.java
+
+import ee.mdd.model.statemachine.StateMachine
+
+
+
+templates('sm') {
+
+  useMacros('commonMacros', '/common/macros')
+  useMacros('macros')
+
+    template('moduleEvent',
+    items: { c -> c.model.findAllRecursiveDown( {StateMachine.isInstance(it) }) },
+    context: { c -> c.putAll( [ component: c.item.component, module: c.item] ) } ) {
+    
+      template('moduleEvent', appendName: true, body: '''<% c.className = ${item.key}StateEvent %> ${macros.generate('moduleEvent', c)}''')
+  
+    }
+    
+
+}
