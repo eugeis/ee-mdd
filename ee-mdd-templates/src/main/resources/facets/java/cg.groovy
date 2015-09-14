392a3999ac5a781fc0c7da15f8879b4bdb823405
elektr0n1k <n.cappelmann@tu-bs.de>
2015-09-14 16:51:13 +0200

    Removed some duplicate references

--- ee-mdd-templates/src/main/resources/facets/java/cg.groovy
+++ ee-mdd-templates/src/main/resources/facets/java/cg.groovy
@@ -20,18 +20,11 @@
  * @author Niklas Cappelmann
  */
 
-def nameToNamespace = ['Backend' : 'com.siemens.ra.cg.pl.common.base.annotations',
-  'JmsDestinationConfig' : 'com.siemens.ra.cg.pl.common.ejb.messaging', 'JmsSender' : 'com.siemens.ra.cg.pl.common.ejb.messaging',
-  'Event' : 'com.siemens.ra.cg.pl.common.base.messaging', 'EventImpl' : 'com.siemens.ra.cg.pl.common.base.messaging.impl',
-  'EventListener' : 'com.siemens.ra.cg.pl.common.base.messaging', 'JmsToEventListener' : 'com.siemens.ra.cg.pl.common.ejb.messaging.jse',
-  'SingleTypeEventListenerBridgeByJms' : 'com.siemens.ra.cg.pl.common.ejb.messaging','ServiceLocator' : 'com.siemens.ra.cg.pl.common.ejb.locator',
-  'LifecycleEvent' : 'com.siemens.ra.cg.pl.common.app.event', 'Traceable' : 'com.siemens.ra.cg.pl.common.base.annotations',
-  'Factory' : 'com.siemens.ra.cg.pl.common.base', 'MultiTypeCdiEventListener' : 'com.siemens.ra.cg.pl.common.base.messaging.impl',
-  'ConnectionMetaEvent' : 'com.siemens.ra.cg.pl.common.base.model.event', 'Backend':'com.siemens.ra.cg.pl.common.base.annotations', 'External' : 'com.siemens.ra.cg.pl.common.base.annotations',
-  'BaseTestCase' : 'com.siemens.ra.cg.pl.common.base.testcase', 'JmsMessagingAdapterTestCase' : 'com.siemens.ra.cg.pl.common.ejb.messaging.jse', 'JmsSendExecutor' : 'com.siemens.ra.cg.pl.common.ejb.messaging',
+def nameToNamespace = ['MultiTypeCdiEventListener' : 'com.siemens.ra.cg.pl.common.base.messaging.impl',
+  'ConnectionMetaEvent' : 'com.siemens.ra.cg.pl.common.base.model.event',
   'BaseEntity' : 'com.siemens.ra.cg.pl.common.base.model', 'IdSetter' : 'com.siemens.ra.cg.pl.common.base.model', 'BaseEntityImpl' : 'com.siemens.ra.cg.pl.common.ejb.model',
-  'AssertionUtils' : 'com.siemens.ra.cg.pl.common.base.assertion', 'ControlguideNotFoundException' : 'com.siemens.ra.cg.pl.common.base.exception',
-  'IdEntity' : 'com.siemens.ra.cg.pl.common.base.model', 'StringUtils' : 'com.siemens.ra.cg.pl.common.base.util', 'TimeUtils' : 'com.siemens.ra.cg.pl.common.base.util'] as TreeMap
+  'ControlguideNotFoundException' : 'com.siemens.ra.cg.pl.common.base.exception',
+  'IdEntity' : 'com.siemens.ra.cg.pl.common.base.model', 'TimeUtils' : 'com.siemens.ra.cg.pl.common.base.util'] as TreeMap
 
 extModule(name: 'CgCommonShared', artifact: 'cg-pl-common-shared') {
   nameToNamespace.each { n, ns ->
