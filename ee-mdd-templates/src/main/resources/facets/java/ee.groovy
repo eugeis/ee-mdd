1a7317c51fe027e9407a85bc1be2529a32161374
z003dc0n <z003dc0n@MD122N1C.ww009.siemens.net>
2016-05-31 15:29:12 +0200

    Work on templates and facets

--- ee-mdd-templates/src/main/resources/facets/java/ee.groovy
+++ ee-mdd-templates/src/main/resources/facets/java/ee.groovy
@@ -20,19 +20,19 @@
 
 extModule(name: 'EeCommonBase', namespace: 'ee.common', artifact: 'ee-common-base') {
   ['EntityIfc' : 'ee.common.model', 'EntityImpl' : 'ee.common.model',
-  'Labeled' : 'ee.common.model', 'ConnectionEvent' : 'ee.common.model', 'LinkedObjectCache' : 'ee.common.cache',
-  'Finder' : 'ee.common.core', 'MultiTypeEventListener' : 'ee.common.core', 'ServiceLocator' : 'ee.common.locator',
-  'MlKey' : 'ee.common.ml', 'MlKeyBuilder' : 'ee.common.ml' , 'MlKeyImpl' : 'ee.common.ml',
-  'Backend' : 'ee.common.annotation', 'Service' : 'ee.common.annotation', 'Environment' : 'ee.common.annotation', 
-  'External' : 'ee.common.annotation', 'SupportsEnvironments' : 'ee.common.annotation', 'Traceable' : 'ee.common.annotation',
-  'SERVER' : 'static ee.common.integ.RuntimeType.*', 'CLIENT' : 'static ee.common.integ.RuntimeType.*', 'LOCAL' : 'static ee.common.integ.ExecutionType.*', 'MEMORY' : 'static ee.common.integ.ExecutionType.*', 
-  'PRODUCTIVE' : 'static ee.common.integ.ExecutionType.*', 'ComparisonUtils' : 'ee.common.util',
-  'LifecycleEvent' : 'ee.common.model',
-  'NotFoundException' : 'ee.common.exception', 'IntegerEntityCache' : 'ee.common.cache', 'StringEntityCache' : 'ee.common.cache',
-  'IntegerCacheOverride' : 'ee.common.cache', 'StringCacheOverride' : 'ee.common.cache',
-  'LinkToSetCache' : 'ee.common.cache', 'LinkToSet' : 'ee.common.model', 'Model' : 'ee.common.annotation',
-  'Controller' : 'ee.common.annotation', 'WorkstationType' : 'ee.common.integ', 'SessionPrincipal' : 'ee.common.model',
-  'RepState' : 'ee.common.model', 'Closeable' : 'ee.common.base', 'Initializable' : 'ee.common.base', 'Config' : 'ee.common.config', 'ConditionVerifier' : 'ee.common.cond', 'StateFlowType' : 'ee.common.statemachine'].each { n, ns ->
+    'Labeled' : 'ee.common.model', 'ConnectionEvent' : 'ee.common.model', 'LinkedObjectCache' : 'ee.common.cache',
+    'Finder' : 'ee.common.core', 'MultiTypeEventListener' : 'ee.common.core', 'ServiceLocator' : 'ee.common.locator',
+    'MlKey' : 'ee.common.ml', 'MlKeyBuilder' : 'ee.common.ml' , 'MlKeyImpl' : 'ee.common.ml',
+    'Backend' : 'ee.common.annotation', 'Environment' : 'ee.common.annotation',
+    'External' : 'ee.common.annotation', 'SupportsEnvironments' : 'ee.common.annotation', 'Traceable' : 'ee.common.annotation',
+    'SERVER' : 'static ee.common.integ.RuntimeType.*', 'CLIENT' : 'static ee.common.integ.RuntimeType.*', 'LOCAL' : 'static ee.common.integ.ExecutionType.*', 'MEMORY' : 'static ee.common.integ.ExecutionType.*',
+    'PRODUCTIVE' : 'static ee.common.integ.ExecutionType.*', 'ComparisonUtils' : 'ee.common.util',
+    'LifecycleEvent' : 'ee.common.model',
+    'NotFoundException' : 'ee.common.exception', 'IntegerEntityCache' : 'ee.common.cache', 'StringEntityCache' : 'ee.common.cache',
+    'IntegerCacheOverride' : 'ee.common.cache', 'StringCacheOverride' : 'ee.common.cache',
+    'LinkToSetCache' : 'ee.common.cache', 'LinkToSet' : 'ee.common.model', 'Model' : 'ee.common.annotation',
+    'Controller' : 'ee.common.annotation', 'WorkstationType' : 'ee.common.integ', 'SessionPrincipal' : 'ee.common.model',
+    'RepState' : 'ee.common.model', 'Closeable' : 'ee.common.base', 'Initializable' : 'ee.common.base', 'Config' : 'ee.common.config', 'ConditionVerifier' : 'ee.common.cond', 'StateFlowType' : 'ee.common.statemachine'].each { n, ns ->
     extType(name: n, namespace: ns)
   }
 }
